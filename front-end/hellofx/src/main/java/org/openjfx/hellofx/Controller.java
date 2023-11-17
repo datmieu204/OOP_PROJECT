ad09ef22deccaeb8faf0609ff4b7c0316d97129d
Tnemo65 <ledacthinh666@gmail.com>
2023-11-17 23:35:31 +0700

    Sidebar

---
+++ front-end/hellofx/src/main/java/org/openjfx/hellofx/Controller.java
@@ -0,0 +1,91 @@
+package org.openjfx.hellofx;
+import javafx.scene.Node;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private AnchorPane pane1, pane2;
+
+    @FXML
+    private ImageView exit, menu;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+
+        pane1.setVisible(false);
+
+        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5),pane2);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+        menu.setOnMouseClicked(event -> {
+
+
+            pane1.setVisible(true);
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.5);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+
+        pane1.setOnMouseClicked(event -> {
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
+            fadeTransition1.setFromValue(0.5);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 -> {
+                pane1.setVisible(false);
+            });
+
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+        });
+    }
+    
+    @FXML
+    public void nextToScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
